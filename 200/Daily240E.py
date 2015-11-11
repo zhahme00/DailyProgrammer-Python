1d0a542fea3fb4a277b8af0bc961d755ff6c493d
Zee Ahmed <zhahme00@gmail.com>
2015-11-11 10:55:43 -0500

    Update Daily240E.py

--- 200/Daily240E.py
+++ 200/Daily240E.py
@@ -1,11 +1,15 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/3s4nyq/20151109_challenge_240_easy_typoglycemia/
+
 import random
 
 def scramble_sentence(sentence):
-    words = sentence.split(' ')
-    for w in words:
-        print(w, scramble_word(w))
+    '''Scrambles each word of a sentence and returns a '''
+    scrambled = map(lambda w: scramble_word(w), sentence.split(' '))
+    return ' '.join(scrambled)
 
 def scramble_word(word):
+    '''Scrambles a word but leaves the first 
+        and last letter of the word intact.'''
     scrambled = []    
     l = []
     for i in range(len(word)):    
@@ -19,6 +23,9 @@ def scramble_word(word):
                 scrambled.append(s)
                 scrambled.append(word[i])
                 l = []
+    else:
+        s = scramble_word_segment(''.join(l))
+        scrambled.append(s)
     return ''.join(scrambled)
 
 def scramble_word_segment(word_segment):
@@ -31,5 +38,12 @@ def scramble_word_segment(word_segment):
         l[current_index], l[new_index] = l[new_index], l[current_index]
     return ''.join(l)
 
-# Does not work, yet!    
-scramble_sentence('"\'Twas the best of time; the worst of time!"')
+if __name__ == '__main__':    
+    i = '''
+    According to a research team at Cambridge University, it doesn't matter in what order the letters in a word are, 
+    the only important thing is that the first and last letter be in the right place. 
+    The rest can be a total mess and you can still read it without a problem.
+    This is because the human mind does not read every letter by itself, but the word as a whole. 
+    Such a condition is appropriately called Typoglycemia.'''
+    s = scramble_sentence(i)
+    print(s)
